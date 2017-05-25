2265960b4adc694d920d15dc7d8ca1773d084b7f
Ruben Maher <r@rkm.id.au>
2017-05-26 08:21:51 +0930

    IDK

--- src/nix/.nixpkgs/darwin-configuration.nix
+++ src/nix/.nixpkgs/darwin-configuration.nix
@@ -21,7 +21,7 @@ in {
     coreutils
     curl
     direnv
-    emacs25
+    emacs
     file
     findutils
     fzf
@@ -43,6 +43,7 @@ in {
     pwgen
     reattach-to-user-namespace
     rsync
+    sift
     silver-searcher
     stow
     syncthing
@@ -53,11 +54,20 @@ in {
     wget
     which
 
+    # not supported on darwin?
+    # mopidy
+    # mopidy-spotify
+    # mopidy-youtube
+    # mopidy-soundcloud
+    mpv
+
     apacheHttpd
+    flow
     jekyll
     lftp
     localPackages.nodePackages."@jasondibenedetto/plop"
     localPackages.nodePackages.eslint-cli
+    localPackages.nodePackages.flow-typed
     localPackages.nodePackages.grunt-cli
     localPackages.nodePackages.imapnotify
     localPackages.nodePackages.react-native-cli
@@ -74,6 +84,17 @@ in {
     php70Packages.phpcs
     watchman
   ];
+
+  nixpkgs.config.allowBroken = true;
+
+  # https://github.com/benhutchins/.dotfiles/blob/master/Mac.md#fix-mac-issues
+
+  # environment.variables = {
+  #   # this is the key to getting aspell to shut up?
+  #   # nix-repl> "${pkgs.aspellDicts.en}/lib/aspell"
+  #   # "/nix/store/6523yd8172rghf51vcknhrrvpd24sg45-aspell-dict-en-2016.06.26-0/lib/aspell"
+  #   ASPELL_CONF = "data-dir ${pkgs.aspellDicts.en}/lib/aspell";
+  # };
 
   environment.shellAliases = {
     emacs = "$(dirname $(dirname $(readlink -f $(which Emacs))))/Applications/Emacs.app/Contents/MacOS/Emacs";
@@ -99,16 +120,44 @@ in {
     '';
   };
 
-  environment.extraOutputsToInstall = [ "man" "dev" "devdoc" ];
+  nixpkgs.config.packageOverrides = pkgs: {
+    sift = lib.overrideDerivation pkgs.sift (attrs: {
+      postInstall = ''
+        install_name_tool -delete_rpath $out/lib -add_rpath $bin $bin/bin/sift
+      '';
+    });
 
-  nixpkgs.config.packageOverrides = pkgs: {    
+    # Flow is in nixpkgs but compilation fails at the moment due to a problem
+    # with ocaml on Darwin.
+    flow = with pkgs; stdenv.mkDerivation rec {
+      name = "flow";
+      version = "0.47.0";
+
+      src = fetchurl {
+        url = "https://github.com/facebook/flow/releases/download/v${version}/${name}-osx-v${version}.zip";
+        sha256 = "08wr9dl9rr6qaf6x4wpwiq0pvll5spyxg0i5nmj1np8p7xjl1wzv";
+      };
+
+      buildInputs = [ unzip ];
+
+      dontBuild = true;
+
+      installPhase = ''
+        mkdir -p "$out/tmp";
+        unzip -d $out/tmp ${src}
+        mkdir -p "$out/bin";
+        mv $out/tmp/flow/flow $out/bin
+        rm -rf "$out/tmp"
+      '';
+    };
+
     syncthing = with pkgs; stdenv.mkDerivation rec {
       name = "syncthing";
-      version = "0.14.25";
+      version = "0.14.27";
 
       src = fetchurl {
         url = "https://github.com/syncthing/syncthing/releases/download/v${version}/syncthing-macosx-amd64-v${version}.tar.gz";
-        sha256 = "19inj8xzhfd7pkqpp9vrvx3nrykvjsysbd9p4kv4l73lnxwriv23";
+        sha256 = "1ami5d1p6cws2m0va7fjqpzbg7rbkxiiv7y7is1h5pgwp302ky1a";
       };
 
       buildInputs = [ gnutar ];
@@ -118,7 +167,6 @@ in {
       installPhase = ''
         mkdir -p "$out/tmp";
         tar xf ${src} -C "$out/tmp" --strip-components 1
-        ls -lha $out/tmp
         mkdir -p "$out/bin";
         mv "$out/tmp/syncthing" "$out/bin"
         rm -rf "$out/tmp"
@@ -130,6 +178,7 @@ in {
     serviceConfig.ProgramArguments = [ "${pkgs.syncthing}/bin/syncthing" "-no-browser" "-no-restart" ];
     serviceConfig.EnvironmentVariables = {
       HOME = "/Users/rkm";
+      STNOUPGRADE = "1"; # disable spammy automatic upgrade check
     };
     serviceConfig.KeepAlive = true;
     serviceConfig.ProcessType = "Background";
