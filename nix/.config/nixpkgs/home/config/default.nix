870f97b991e240e1fe7283a4b16c456b16634b27
Ruben Maher <r@rkm.id.au>
2017-11-13 08:53:50 +1030

    Don't install stuff on darwin that it can't deal with

--- nix/.config/nixpkgs/home/config/default.nix
+++ nix/.config/nixpkgs/home/config/default.nix
@@ -10,25 +10,26 @@
   programs.home-manager.path = "<home-manager>";
 
   home.packages = with pkgs; [
-    chromium
     fd
-    firefox
     fortune
-    gimp
     gitAndTools.git-crypt
     gitAndTools.gitFull
     haskellPackages.ShellCheck
-    icedtea8_web # iDRAC administration
     isync
     jhead
     jq
-    libreoffice
-    mpv
     ncmpcpp
     pass
     pinentry
-    python27Packages.syncthing-gtk
     speedtest-cli
+  ] ++ lib.optionals stdenv.isLinux [ 
+    firefox
+    chromium
+    gimp
+    mpv
+    icedtea8_web # iDRAC administration
+    python27Packages.syncthing-gtk
+    libreoffice 
     steam
     wine
   ] ++ (with local-packages; [
