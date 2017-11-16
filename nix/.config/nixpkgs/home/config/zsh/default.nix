a552266ef77a155cf23674e1f633aefe8ffd6364
Ruben Maher <r@rkm.id.au>
2017-11-17 08:33:35 +1030

    Move histfile to actual zshrc

--- nix/.config/nixpkgs/home/config/zsh/default.nix
+++ nix/.config/nixpkgs/home/config/zsh/default.nix
@@ -282,26 +282,6 @@ in {
     export FZF_TMUX="1"
     export FZF_ALT_C_COMMAND="${pkgs.fd}/bin/fd --type d --hidden --follow"
     export FZF_CTRL_T_COMMAND="${pkgs.fd}/bin/fd --type f --hidden --follow"
-
-    export RKM_HIST_DIR="''${HOME}/history/zsh"
-    test -d "$RKM_HIST_DIR" || mkdir -p "$RKM_HIST_DIR"
-    export HISTFILE="''${RKM_HIST_DIR}/$(date -u +%Y-%m-%d.%H.%M.%S)_$(hostname)_$$"
-    HISTSIZE=1000000
-    SAVEHIST=$HISTSIZE
-
-    setopt BANG_HIST                 # Treat the '!' character specially during expansion.
-    setopt EXTENDED_HISTORY          # Write the history file in the ":start:elapsed;command" format.
-    setopt INC_APPEND_HISTORY        # Write to the history file immediately, not when the shell exits.
-    setopt SHARE_HISTORY             # Share history between all sessions.
-    setopt HIST_EXPIRE_DUPS_FIRST    # Expire duplicate entries first when trimming history.
-    setopt HIST_IGNORE_DUPS          # Don't record an entry that was just recorded again.
-    setopt HIST_IGNORE_ALL_DUPS      # Delete old recorded entry if new entry is a duplicate.
-    setopt HIST_FIND_NO_DUPS         # Do not display a line previously found.
-    setopt HIST_IGNORE_SPACE         # Don't record an entry starting with a space.
-    setopt HIST_SAVE_NO_DUPS         # Don't write duplicate entries in the history file.
-    setopt HIST_REDUCE_BLANKS        # Remove superfluous blanks before recording entry.
-    setopt HIST_VERIFY               # Don't execute immediately upon history expansion.
-    setopt HIST_BEEP                 # Beep when accessing nonexistent history.
   '';
 
   home.file.".zshrc".source = pkgs.writeText "zshrc" ''
@@ -429,6 +409,26 @@ in {
     _fzf_compgen_dir() {
       ${pkgs.fd}/bin/fd --type d --hidden --follow --exclude ".git" . "''${1}"
     }
+
+    export RKM_HIST_DIR="''${HOME}/history/zsh"
+    test -d "$RKM_HIST_DIR" || mkdir -p "$RKM_HIST_DIR"
+    export HISTFILE="''${RKM_HIST_DIR}/$(date -u +%Y-%m-%d.%H.%M.%S)_$(hostname)_$$"
+    HISTSIZE=1000000
+    SAVEHIST=$HISTSIZE
+
+    setopt BANG_HIST                 # Treat the '!' character specially during expansion.
+    setopt EXTENDED_HISTORY          # Write the history file in the ":start:elapsed;command" format.
+    setopt INC_APPEND_HISTORY        # Write to the history file immediately, not when the shell exits.
+    setopt SHARE_HISTORY             # Share history between all sessions.
+    setopt HIST_EXPIRE_DUPS_FIRST    # Expire duplicate entries first when trimming history.
+    setopt HIST_IGNORE_DUPS          # Don't record an entry that was just recorded again.
+    setopt HIST_IGNORE_ALL_DUPS      # Delete old recorded entry if new entry is a duplicate.
+    setopt HIST_FIND_NO_DUPS         # Do not display a line previously found.
+    setopt HIST_IGNORE_SPACE         # Don't record an entry starting with a space.
+    setopt HIST_SAVE_NO_DUPS         # Don't write duplicate entries in the history file.
+    setopt HIST_REDUCE_BLANKS        # Remove superfluous blanks before recording entry.
+    setopt HIST_VERIFY               # Don't execute immediately upon history expansion.
+    setopt HIST_BEEP                 # Beep when accessing nonexistent history.
 
     source "${zsh-syntax-highlighting}/zsh-syntax-highlighting.zsh"
 
