6e6308d67a1c2a6e7bafdd3973394c63f3828354
Ruben Maher <ruben@maher.fyi>
2017-12-29 21:17:08 +1030

    Bunch of stuff

--- nix/.config/nixpkgs/home/config/default.nix
+++ nix/.config/nixpkgs/home/config/default.nix
@@ -11,42 +11,44 @@
   programs.home-manager.enable = true;
   programs.home-manager.path = "<home-manager>";
 
-  home.file.".psqlrc".source = pkgs.writeText "psqlrc" ''
-    \x auto
-  '';
-
   xdg.enable = true;
 
-  home.packages = with pkgs; [
-    fd
-    fortune
-    gitAndTools.git-crypt
-    gitAndTools.gitFull
-    haskellPackages.ShellCheck
-    isync
-    jhead
-    jq
-    ncmpcpp
-    pass
-    pinentry
-    speedtest-cli
-  ] ++ lib.optionals stdenv.isLinux [ 
-    firefox-bin
-    wine
-    winetricks
-    chromium
-    gimp
-    mpv
-    icedtea8_web # iDRAC administration
-    nextcloud-client
-    # python27Packages.syncthing-gtk
-    libreoffice 
-    steam
-    wine
-    emacs
-  ] ++ (with local-packages; [
-    # emacs-git # broken :(
-    nixfmt
-    riot
-  ]);
+  home = {
+    file.".psqlrc".source = pkgs.writeText "psqlrc" ''
+      \x auto
+    '';
+
+    packages = with pkgs; [
+      fd
+      fortune
+      gitAndTools.git-crypt
+      gitAndTools.gitFull
+      haskellPackages.ShellCheck
+      isync
+      jhead
+      jq
+      ncmpcpp
+      pass
+      pinentry
+      speedtest-cli
+    ] ++ lib.optionals stdenv.isLinux [
+      firefox-bin
+      wine
+      winetricks
+      chromium
+      gimp
+      mpv
+      icedtea8_web # iDRAC administration
+      nextcloud-client
+      # python27Packages.syncthing-gtk
+      libreoffice
+      steam
+      wine
+      emacs
+    ] ++ (with local-packages; [
+      # emacs-git # broken :(
+      nixfmt
+      riot
+    ]);
+  };
 }
