8076a87ba20043181b426c294e2710458a20f5fb
Ruben Maher <r@rkm.id.au>
2018-01-04 07:59:42 +1030

    nix: urlwatch user service

---
+++ nix/.config/nixpkgs/home/config/urlwatch/default.nix
@@ -0,0 +1,69 @@
+{ lib, pkgs, ... }:
+
+{
+  home.file.".config/urlwatch/urlwatch.yaml".source = pkgs.writeText "urlwatch.yaml" (builtins.toJSON {
+    display = {
+      error = true;
+      new = true;
+      unchanged = true;
+    };
+    report = {
+      email = {
+        enabled = true;
+        from = "ruben@maher.fyi";
+        to = "ruben@maher.fyi";
+        method = "sendmail";
+        sendmail = {
+          path = "/run/wrappers/bin/sendmail";
+        };
+      };
+    };
+  });
+
+  home.file.".config/urlwatch/urls.yaml".source = pkgs.writeText "urls.yaml" ''
+    kind: url
+    url: https://jpf.org.au/language/for-learners/jlpt/
+    filter: element-by-class:wpb_wrapper
+    ---
+    kind: url
+    url: http://jafa.asn.au/index.php/jlpt
+    filter: element-by-class:item-page
+    ---
+    kind: url
+    url: https://pypi.python.org/pypi/awsebcli
+    filter: element-by-id:changelog
+  '';
+
+  systemd.user = {
+    services.urlwatch = {
+      Unit = {
+        Description = "Run urlwatch";
+        After = [ "network.target" ];
+      };
+
+      Service = {
+        Type = "forking";
+        ExecStart = "${pkgs.urlwatch}/bin/urlwatch";
+      };
+
+      Install = {
+        WantedBy = [ "default.target" ];
+      };
+    };
+
+    timers.urlwatch = {
+      Unit = {
+        Description = "Run urlwatch";
+        PartOf = [ "urlwatch.service" ];
+      };
+
+      Timer = {
+        OnCalendar = "hourly";
+      };
+
+      Install = {
+        WantedBy = [ "timers.target" ];
+      };
+    };
+  };
+}
--- nix/.config/nixpkgs/nixops/realms/tomoyo.maher.fyi/config/home.nix
+++ nix/.config/nixpkgs/nixops/realms/tomoyo.maher.fyi/config/home.nix
@@ -2,9 +2,12 @@
 
 {
   home-manager.users = {
-    eqyiel = lib.mkMerge [(import ../../../../home/config/syncthing {
-      inherit lib pkgs;
-      actualHostname = config.networking.hostName;
-    })];
+    eqyiel = lib.mkMerge [
+      (import ../../../../home/config/syncthing {
+        inherit lib pkgs;
+        actualHostname = config.networking.hostName;
+      })
+      (import ../../../../home/config/urlwatch)
+    ];
   };
 }
