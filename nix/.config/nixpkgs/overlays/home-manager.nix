7356806bd454b5d495673594e5d971ca3dab3b45
Ruben Maher <ruben@maher.fyi>
2018-06-25 07:18:14 +0930

    Fix home manager spell

--- nix/.config/nixpkgs/overlays/home-manager.nix
+++ nix/.config/nixpkgs/overlays/home-manager.nix
@@ -1,19 +1,24 @@
 self: super: rec {
-  home-manager-src = (super.callPackage ({ stdenv, fetchgit }: stdenv.mkDerivation {
-    name = "home-manager";
-    src = fetchgit {
-      url = ../../../.nix-defexpr/home-manager;
-      rev = "5641ee3f942e700de35b28fc879b0d8a10a7a1fe";
-      sha256 = "0bqzwczbr5c2y3ms7m7ly0as9zsnqwljq61ci2y2gbqzw3md1x2j";
-    };
+  home = {
+    nixpkgs = (super.callPackage ../home/lib/nixpkgs.nix {});
 
-    dontBuild = true;
-    preferLocalBuild = true;
+    home-manager-src = (super.callPackage ({ stdenv, fetchFromGitHub }: stdenv.mkDerivation {
+      name = "home-manager";
+      src = fetchFromGitHub {
+        owner = "rycee";
+        repo = "home-manager";
+        rev = "5641ee3f942e700de35b28fc879b0d8a10a7a1fe";
+        sha256 = "0bqzwczbr5c2y3ms7m7ly0as9zsnqwljq61ci2y2gbqzw3md1x2j";
+      };
 
-    installPhase = ''
-      cp -a . $out
-    '';
-  }) {});
+      dontBuild = true;
+      preferLocalBuild = true;
 
-  home-manager = import "${self.home-manager-src}/overlay.nix" self super;
+      installPhase = ''
+        cp -a . $out
+      '';
+    }) {});
+
+    home-manager = import "${self.home.home-manager-src}/overlay.nix" self super;
+  };
 }
