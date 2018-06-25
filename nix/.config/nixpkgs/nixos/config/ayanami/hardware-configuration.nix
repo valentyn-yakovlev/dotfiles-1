b37b911683425a43d97b78a35c4e682db5fc135e
Ruben Maher <ruben@maher.fyi>
2018-06-26 09:29:50 +0930

    Updates

--- nix/.config/nixpkgs/nixos/config/ayanami/hardware-configuration.nix
+++ nix/.config/nixpkgs/nixos/config/ayanami/hardware-configuration.nix
@@ -1,36 +1,34 @@
 { config, lib, pkgs, ... }:
 
 {
-  imports = [
-    <nixpkgs/nixos/modules/installer/scan/not-detected.nix>
-  ];
+  imports = [<nixpkgs/nixos/modules/installer/scan/not-detected.nix>];
 
   boot = {
-    kernelModules = [ "kvm-intel" ];
-    extraModulePackages = [];
     initrd.availableKernelModules = [
+      "xhci_pci"
       "ehci_pci"
       "ahci"
       "usb_storage"
       "sd_mod"
       "rtsx_pci_sdmmc"
     ];
+    kernelModules = ["kvm-intel"];
+    extraModulePackages = [];
   };
+
 
   fileSystems = {
     "/" = {
-      device = "/dev/mapper/vg-root";
+      device = "/dev/disk/by-uuid/a33b25a5-1501-413f-9c41-92062a8609fd";
       fsType = "ext4";
     };
     "/boot" = {
-       device = "/dev/disk/by-uuid/33C7-BD51";
-       fsType = "vfat";
+      device = "/dev/disk/by-uuid/5ad5552f-3d6a-4b3e-ab46-b8c2324f3d92";
+      fsType = "ext4";
     };
-  };
+  }
 
-  swapDevices = [{ device = "/dev/mapper/vg-swap"; }];
-
-  hardware.cpu.intel.updateMicrocode = true;
-
+  swapDevices = [];
   nix.maxJobs = lib.mkDefault 4;
+  powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
 }
