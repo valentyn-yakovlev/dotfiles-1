f5ffe71b56c635eec1615fb91d098d544a1d75d6
Ruben Maher <ruben@maher.fyi>
2018-06-25 11:12:03 +0930

    darwin: actually works with builtins.fetchGit

--- nix/.config/nixpkgs/home/lib/nixpkgs.nix
+++ nix/.config/nixpkgs/home/lib/nixpkgs.nix
@@ -1,12 +1,11 @@
-{ stdenv, fetchgit }:
+{ stdenv }:
 
 stdenv.mkDerivation rec {
   name = "nixpkgs";
 
-  src = fetchgit {
+  src = fetchGit {
     url = ../../../../.nix-defexpr/nixpkgs;
     rev = "a8c71037e041725d40fbf2f3047347b6833b1703";
-    sha256 = "1z4cchcw7qgjhy0x6mnz7iqvpswc2nfjpdynxc54zpm66khfrjqw";
   };
 
   dontBuild = true;
