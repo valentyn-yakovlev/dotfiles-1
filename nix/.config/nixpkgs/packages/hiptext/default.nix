70f6869b3d43d427ccd4ec08b3fa61f8998df4af
Ruben Maher <ruben@maher.fyi>
2018-06-28 14:41:44 +0930

    hiptext: Fix package

--- nix/.config/nixpkgs/packages/hiptext/default.nix
+++ nix/.config/nixpkgs/packages/hiptext/default.nix
@@ -1,19 +1,19 @@
 { stdenv
 , autoreconfHook
 , fetchFromGitHub
-, libav
 , freetype
 , gflags
 , glog
+, libav
 , libjpeg
 , libpng
+, makeWrapper
 , pkgconfig
 , ragel
 }:
 
 stdenv.mkDerivation rec {
-  name = "hiptext-" + version;
-  version = "HEAD";
+  name = "hiptext";
 
   # nix-prefetch-git git@github.com:jart/hiptext.git --rev HEAD
   src = fetchFromGitHub {
@@ -23,14 +23,30 @@ stdenv.mkDerivation rec {
     sha256 = "0m8vv103nj7pbz5lzzcgjdkasgygjv52zaxrh63yvd85c6pl339v";
   };
 
+  postInstall = ''
+    # Without a --font argument, hiptext will try to use DejaVuSansMono.ttf from
+    # the source directory.  If that file could not be found and there was no
+    # --font argument provided, the program will crash.  So wrap with a default
+    # --font argument.
+    #
+    # https://github.com/jart/hiptext/blob/da18b54c614beb74b7cb8671ea501caae2f8e85f/src/font.cc#L14
+    # https://github.com/jart/hiptext/issues/12
+    mkdir -p $out/share/hiptext
+    cp $src/DejavuSansMono.ttf $out/share/hiptext
+
+    wrapProgram $out/bin/hiptext \
+      --add-flags "--font $out/share/hiptext/DejaVuSansMono.ttf"
+  '';
+
   buildInputs = [
     autoreconfHook
-    libav
     freetype
     gflags
     glog
+    libav
     libjpeg
     libpng
+    makeWrapper
     pkgconfig
     ragel
   ];
